c1335abb037867f9a4c1a7b52772b615ad085ef5
justin siebenhaar <justin.siebenhaar@utah.edu>
2020-08-26 08:29:11 -0600

    vending machine part 2 update

--- VendingMachine1/VendingMachine1/main.cpp
+++ VendingMachine1/VendingMachine1/main.cpp
@@ -9,25 +9,65 @@
 #include <iostream>
 using namespace std;
 int main(int argc, const char * argv[]) {
-    int itemCost, amtPaid, change, quarters, dimes, nickels, pennies;
+    int itemCost, amtPaid, change, quarters, dimes, nickels, pennies, extra;
+    int quartersTotal=2;
+    int dimesTotal=2;
+    int nicklesTotal=2;
+    int penniesTotal=2;
+    cout << "how many quarters?" << endl;
+    cin >> quartersTotal;
+    cout << "how many dimes?" << endl;
+    cin >> dimesTotal;
+    cout << "how many nickles?" << endl;
+    cin >> nicklesTotal;
+    cout << "how many pennies?" << endl;
+    cin >> penniesTotal;
+    
+    //get amounts of item and amount paid
     cout << "enter item price in cents: " << endl;
     cin >> itemCost;
     cout << "enter amount paid in cents: " << endl;
     cin >> amtPaid;
     change = amtPaid - itemCost;
-    quarters = change/25;
-    change = change%25;
-    dimes = change/10;
-    change = change%10;
-    nickels = change/5;
-    change = change%5;
-    pennies = change/1;
-    change = change%1;
     
-    cout << "Change: " << amtPaid - itemCost << endl
-    <<"   Quarters: "<< quarters << endl
-    << "    Dimes: "<< dimes << endl
-    << "    Nickles: "<< nickels << endl
-    << "    Pennies: "<< pennies << endl;
+    //calculate change
+    quarters = change/25;
+    change %= 25;
+    if(quarters>quartersTotal){
+        extra = (quarters - quartersTotal)*25;
+        change+=extra;
+        quarters=quartersTotal;
+    };
+    dimes = change/10;
+    change %= 10;
+    if (dimes>dimesTotal) {
+        extra = (dimes - dimesTotal)*10;
+        change+=extra;
+        dimes=dimesTotal;
+    }
+    nickels = change/5;
+    change %= 5;
+    if (nickels>nicklesTotal) {
+        extra = (nickels - nicklesTotal)*5;
+        change+=extra;
+        nickels=nicklesTotal;
+    }
+    pennies = change/1;
+    change %= 1;
+    if (pennies>penniesTotal) {
+        extra = (pennies - penniesTotal)*1;
+        change+=extra;
+        pennies=penniesTotal;
+    }
+    //notice if not enough change
+    if (change>0) {
+        cout << "not enough change, sorry!"<< endl;
+    }else{
+        cout << "Change: " << amtPaid - itemCost << endl
+        << "    Quarters: "<< quarters << endl
+        << "    Dimes: "<< dimes << endl
+        << "    Nickles: "<< nickels << endl
+        << "    Pennies: "<< pennies << endl;
+    };
     return 0;
 }
