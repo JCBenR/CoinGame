6889f8f2e7d738580621fe7fe7331fd0f77a6d56
justin siebenhaar <justin.siebenhaar@utah.edu>
2020-09-16 08:32:33 -0600

    added assert tests to makeOwnVector

--- makeYourOwnVector/makeYourOwnVector/main.cpp
+++ makeYourOwnVector/makeYourOwnVector/main.cpp
@@ -19,56 +19,38 @@ int main(int argc, const char * argv[]) {
     
     Vector firstArr = makeVector(6);
     pushBack(firstArr, 10);
-    //assert(firstArr.capacity == 6);
-    cout<<firstArr.arrStart;
-    cout<<firstArr.arrStart[0]<<endl;
-    cout<<"the size is: "<<firstArr.size<<endl;
-    cout<<"the capacity is: "<<firstArr.capacity<<endl;
-    pushBack(firstArr, 45);
-    cout<<"the size is: "<<firstArr.size<<endl;
-    cout<<"location contains: "<<firstArr.arrStart[1]<<endl;
-    popBack(firstArr);
-    cout<<"the size is: "<<firstArr.size<<endl;
-    cout<<"location contains: "<<firstArr.arrStart[1]<<endl;
-    cout<<get(firstArr, 0)<<endl;
-    set(firstArr, 1, 33);
-    cout<<get(firstArr, 1)<<endl;
-    cout<<"the capacity is: "<<firstArr.capacity<<endl;
-    pushBack(firstArr, 15);
-    cout<<"the capacity is: "<<firstArr.capacity<<endl;
-    pushBack(firstArr, 16);
-
-    cout<<"the capacity is: "<<firstArr.capacity<<endl;
-    pushBack(firstArr, 17);
+    //TEST THAT CAPACITY IS 6, AS SET ABOVE AND THAT SIZE IS 1
+    assert(firstArr.capacity == 6 && firstArr.size == 1);
+    
+    //SHOW WHAT ADDRESS IS OF ARRAY START
     cout<<firstArr.arrStart<<endl;
+    //ADD 7 MORE ITEMS TO THE ARRAY, WHICH SHOULD TRIGGER IT TO INCREASE AND GROW TO 12
+    for(int i = 0; i < 7; i++){
+        pushBack(firstArr, 45);
+    }
     cout<<"the capacity is: "<<firstArr.capacity<<endl;
-    cout<<firstArr.arrStart[3]<<endl;
-    pushBack(firstArr, 18);
-    pushBack(firstArr, 19);
-    pushBack(firstArr, 20);
-    pushBack(firstArr, 21);
-    pushBack(firstArr, 22);
-    pushBack(firstArr, 23);
-//    growVector(firstArr);
-    cout<<firstArr.arrStart;
-    cout<<"the capacity is: "<<firstArr.capacity<<endl;
-    pushBack(firstArr, 21);
-    pushBack(firstArr, 22);
-    pushBack(firstArr, 23);
-    pushBack(firstArr, 21);
-    pushBack(firstArr, 22);
-    pushBack(firstArr, 23);
-    pushBack(firstArr, 22);
-    pushBack(firstArr, 23);
-    cout<<firstArr.arrStart;
-    cout<<"the capacity is: "<<firstArr.capacity<<endl;
-    cout<<get(firstArr, 14)<<endl;
-    set(firstArr, 14, 81);
-    cout<<get(firstArr, 14)<<endl;
-    //assert(firstArr.capacity > 15);
+    assert(firstArr.capacity == 12 && firstArr.size == 8);
     
+    //REMOVE ONE AND TEST THAT SIZE IS NOW 7;
+    popBack(firstArr);
+    assert(firstArr.capacity == 12 && firstArr.size == 7);
+    cout<<"the size is: "<<firstArr.size<<endl;
     
-    
+    //TEST THAT FIRST INDEX IS 45, THEN CHANGE FIRST INDEX TO 33 AND TEST
+    cout<<"ARRAY index 1 is: "<<get(firstArr, 1)<<endl;
+    assert(firstArr.arrStart[1]==45);
+    set(firstArr, 1, 33);
+    cout<<"ARRAY index 1 is: "<<get(firstArr, 1)<<endl;
+    assert(firstArr.arrStart[1]==33);
+
+    //ADD TEN MORE ITEMS TO ARRAY AND VERIFY THAT CAPACITY IS NOW 24, SIZE 17, AND ADDRESS IS DIFFERENT
+    for(int i = 0; i < 10; i++){
+        pushBack(firstArr, 11);
+    }
+    cout<<"the size is: "<<firstArr.size<<" AND the capacity is: "<<firstArr.capacity<<endl;
+    assert(firstArr.capacity == 24 && firstArr.size == 17);
+    cout<<"the size is: "<<firstArr.size<<" AND the capacity is: "<<firstArr.capacity<<endl;
+    cout<<firstArr.arrStart<<endl;
     
     return 0;
 }
