fc859b5189c46676d605c9bdac79d4fa468809fd
justin siebenhaar <justin.siebenhaar@utah.edu>
2020-09-17 15:13:39 -0600

    class fraction overload

---
+++ ClassFractionOverload/ClassFractionOverload/main.cpp
@@ -0,0 +1,61 @@
+//
+//  main.cpp
+//  ClassFraction
+//
+//  Created by Justin Siebenhaar on 9/16/20.
+//  Copyright © 2020 Justin Siebenhaar. All rights reserved.
+//
+
+#include <iostream>
+#include "fraction.hpp"
+
+
+
+using namespace std;
+
+//int main(int argc, const char * argv[]) {
+//
+//    Fraction test(2,4);
+//    Fraction rhs1(1,3);
+////    cout<<test.getNumerator()<<endl;
+////    cout<<test.getDenominator()<<endl;
+//    
+//    cout<<test.getNumerator()<<"/"<<test.getDenominator()<<endl;
+//    cout<<rhs1.getNumerator()<<"/"<<rhs1.getDenominator()<<endl;
+//    Fraction testPlusMethod = test.plus(rhs1);
+//    cout<<testPlusMethod.getNumerator()<<endl;
+//    cout<<testPlusMethod.getDenominator()<<endl;
+//    assert(testPlusMethod.getNumerator() == 5);
+//    assert(testPlusMethod.getDenominator() == 6);
+//    
+//    Fraction rhtest2(5,20);
+//    Fraction lhtest2(2,8);
+//    Fraction negtest1(-2,8);
+//    
+//    //MINUS TESTS
+//    Fraction minusTest = lhtest2.minus(rhs1);
+//    Fraction minustestneg = lhtest2.minus(negtest1);
+//    
+//    
+//    
+//    //MULTIPLY TEST
+//    Fraction multiTest = lhtest2.times(rhs1);
+//    Fraction multiTestNeg = lhtest2.times(negtest1);
+//    
+//    
+//    //DIVISION TEST
+//    Fraction dividTest = test.dividedBy(rhs1);
+//    assert(dividTest.getDenominator()== 1);
+//    assert(dividTest.getNumerator()== 2);
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    return 0;
+//}
